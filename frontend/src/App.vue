0aef4f5bd75017b462f2786147ebd4a4aa7e2d54
Lukas Hirt <info@hirt.cz>
2025-02-07 16:27:36 +0000

    feat(frontend): add sign in

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,13 +12,20 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar'
+import { RouterView } from 'vue-router'
+import { useAuthStore } from './feature/auth/store/auth'
+import { Toaster } from './components/ui/toast'
+
+const authStore = useAuthStore()
 </script>
 
 <template>
   <SidebarProvider>
-    <NavigationSidebar />
+    <NavigationSidebar v-if="authStore.isAuthenticated" />
+
     <SidebarInset>
       <header
+        v-if="authStore.isAuthenticated"
         class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
       >
         <div class="flex items-center gap-2 px-4">
@@ -33,6 +40,10 @@ import {
           </Breadcrumb>
         </div>
       </header>
+
+      <RouterView />
     </SidebarInset>
   </SidebarProvider>
+
+  <Toaster />
 </template>
