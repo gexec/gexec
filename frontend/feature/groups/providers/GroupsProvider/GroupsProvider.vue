e58d3549234fae987127b9b2c8d934d829ae4f7f
Thomas Boerger <thomas@webhippie.de>
2025-04-09 00:46:42 +0200

    feat: add simple listing for users and groups

---
+++ frontend/feature/groups/providers/GroupsProvider/GroupsProvider.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { provideGroupsContext } from '.'
+import { type Group, listGroups } from '../../../../client'
+
+const groups = ref<Group[]>([])
+const isLoading = ref(false)
+
+async function loadGroups() {
+  isLoading.value = true
+
+  const { data, error } = await listGroups({
+    query: {
+        // search: '',
+        sort: 'name',
+        order: 'asc',
+        limit: 1000,
+        offset: 0,
+     },
+  })
+
+  if (error) {
+    isLoading.value = false
+    throw error
+  }
+
+  groups.value = data.groups
+  isLoading.value = false
+}
+
+function addGroup(group: Group) {
+  groups.value = [group, ...groups.value]
+}
+
+provideGroupsContext({
+  groups,
+  isLoading,
+  loadGroups,
+  addGroup,
+})
+</script>
+
+<template>
+  <slot />
+</template>
